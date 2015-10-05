4e8d950d46a38beb102f5e4f792ee008811f93ba
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-10-04 22:09:07 -0300

    + validacao para produtos

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CodeCommerce\Http\Requests;
+
+use CodeCommerce\Http\Requests\Request;
+
+class ProductRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'          => 'required | min:5 | max: 80',
+            'description'   => 'required | min:2',
+            'price'         => 'required | regex:/^\d*(\.\d{2})?$/',
+            'featured'      => 'required',
+            'recommend'     => 'required'
+        ];
+    }
+
+//    public function messages()
+//    {
+//        return [
+//            'name.required' => 'O campo Name é obrigatório'
+//        ];
+//    }
+}
