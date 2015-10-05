9fe3edff8c9521522d0674ba544d6e2d71764b69
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-10-04 23:10:38 -0300

    * tentar corrigir validacao preco

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -26,7 +26,7 @@ class ProductRequest extends Request
         return [
             'name'          => 'required | min:5 | max: 80',
             'description'   => 'required | min:2',
-            'price'         => 'required | regex:/^\d*(\.\d{2})?$/',
+            'price'         => 'required | min:0.01 | regex:/^\d*(\.\d{2})?$/',
             'featured'      => 'required',
             'recommend'     => 'required'
         ];
